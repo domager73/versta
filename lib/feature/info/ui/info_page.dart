af9c7d4958f01315d6d91c6a6a38788ea100c26d
Nikolay-leukhin <kolaleuhin@gmail.com>
2024-04-07 10:56:40 +0300

    info data

--- lib/feature/info/ui/info_page.dart
+++ lib/feature/info/ui/info_page.dart
@@ -13,24 +13,50 @@ class _InfoPageState extends State<InfoPage> {
     return Container(
       child: Wrap(
         children: [
-          Container(
-            padding: const EdgeInsets.all(32),
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(40)),
-            child: Column(
-              children: [
-                Image.asset("icon.svg"),
-                const SizedBox(
-                  height: 23,
-                ),
-                Text("Оценка"),
-                const SizedBox(
-                  height: 23,
-                ),
-                Text("Дадим экспертную оценку текущим знаниям вашего ребенка"),
-                
-              ],
-            ),
-          )
+          InfoItemWidget(
+              imagePath: "icon.svg", title: "Bebra", description: "Bebra" * 10),
+          InfoItemWidget(
+              imagePath: "icon.svg", title: "Bebra", description: "Bebra" * 10),
+          InfoItemWidget(
+              imagePath: "icon.svg", title: "Bebra", description: "Bebra" * 10),
+          InfoItemWidget(
+              imagePath: "icon.svg", title: "Bebra", description: "Bebra" * 10),
+        ],
+      ),
+    );
+  }
+}
+
+class InfoItemWidget extends StatelessWidget {
+  final String imagePath;
+  final String title;
+  final String description;
+
+  const InfoItemWidget(
+      {super.key,
+      required this.imagePath,
+      required this.title,
+      required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(32),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(40)),
+      child: Column(
+        children: [
+          Image.asset("icon.svg"),
+          const SizedBox(
+            height: 23,
+          ),
+          const Text("Оценка"),
+          const SizedBox(
+            height: 23,
+          ),
+          const Text("Дадим экспертную оценку текущим знаниям вашего ребенка"),
+          const SizedBox(
+            height: 23,
+          ),
         ],
       ),
     );
